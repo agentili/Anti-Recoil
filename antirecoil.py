391f36149d2e6830b191f28cdc7f556677327f81
Alessandro Gentili <alessandrogentili_7@hotmail.com>
2023-08-27 11:01:59 +0200

    Adding mouse module Adding coloring logs Parameter adjustment

--- antirecoil.py
+++ antirecoil.py
@@ -1,17 +1,17 @@
 
-import pyautogui
-import time
-import win32api
 import random
+import time
+
 import keyboard
 import mouse
+from termcolor import colored
 
 ## Configuration
 # Set the horizontal limit: 5 means a maximum of 5 pixels to the left or to the right every shot
-horizontal_range = 3
+horizontal_range = 5
 # Set the minimum and maximum amount of pixels to move the mouse every shot
-min_vertical = 4
-max_vertical = 6
+min_vertical = 8
+max_vertical = 10
 # Set the minimum and maximum amount of time in seconds to wait until moving the mouse again
 min_firerate = 0.03
 max_firerate = 0.05
@@ -19,10 +19,6 @@ max_firerate = 0.05
 toggle_button = 'end'
 # Set whether the anti-recoil is enabled by default
 enabled = False
-
-def is_mouse_down():    # Returns true if the left mouse button is pressed
-    lmb_state = win32api.GetKeyState(0x01)
-    return lmb_state < 0
 
 # Some prints for startup
 print("Anti-recoil script started!")
@@ -40,11 +36,15 @@ while True:
         if last_state:
             enabled = not enabled
             if enabled:
-                print("Anti-recoil ENABLED")
+                print("Anti-recoil", colored('****ENABLED****', 'green'))
             else:
-                print("Anti-recoil DISABLED")
-    
-    if is_mouse_down() and enabled:
+                print("Anti-recoil", colored('DISABLED', 'red'))
+
+    if mouse.is_pressed() and enabled:
+        if count == 0:
+            keyboard.press_and_release('Alt')
+            count = 1
+
         # Offsets are generated every shot between the min and max config settings
         offset_const = 1000
         horizontal_offset = random.randrange(-horizontal_range * offset_const, horizontal_range * offset_const, 1) / offset_const
@@ -53,10 +53,8 @@ while True:
         # print("Move: " , horizontal_offset , " " , vertical_offset)
 
         # Move the mouse with these offsets
-        win32api.mouse_event(0x0001, int(horizontal_offset), int(vertical_offset))
+        # win32api.mouse_event(0x0001, int(horizontal_offset), int(vertical_offset))
+        mouse.move(int(horizontal_offset), int(vertical_offset), False)
 
-        # Generate random time offset with the config settings
-        time_offset = random.randrange(min_firerate * offset_const, max_firerate * offset_const, 1) / offset_const
-        time.sleep(time_offset)
     # Delay for the while loop
-    time.sleep(0.001)
+    time.sleep(0.01)
